2ba0d360c285da64dc1e1eb6d4a892885f3cc510
David Guttman <david@davidguttman.com>
2014-11-30 14:43:56 -0800

    added wave-pendulum for preload

--- slides/preload.js
+++ slides/preload.js
@@ -1,3 +1,13 @@
+var Pendulum = require('wave-pendulum')
+
 module.exports = function(el) {
-  el.innerHTML = 'harro!'
+  el.innerHTML = ''
+  pendulum = new Pendulum(
+    { width: window.innerWidth
+    , height: window.innerHeight
+    , background: '#0E1321'
+    , foreground: '#FDF3B6'
+    }
+  )
+  el.appendChild(pendulum.el)
 }
