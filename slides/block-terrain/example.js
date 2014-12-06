7cb2bb6a9b17f50cf34e6ac1b714f741146d496f
David Guttman <david@davidguttman.com>
2014-12-06 15:40:44 -0800

    perlin example is a bit better

--- slides/block-terrain/example.js
+++ slides/block-terrain/example.js
@@ -1,16 +1,11 @@
 var raf = require('raf')
 var Color = require('color')
-var Isomer = require('isomer')
-var ease = require('ease-component')
+
 var Simplex = require('perlin-simplex')
 
 var simplex = new Simplex()
 
-var Point = Isomer.Point
-var Shape = Isomer.Shape
-
 var cOrange = Color('#F27300')
-cOrange.darken(0.5)
 
 module.exports = function(el) {
   el.innerHTML = ''
@@ -22,57 +17,31 @@ module.exports = function(el) {
   var width = canvas.width = window.innerWidth
   var height = canvas.height = window.innerHeight
 
-  var duration = 2000
+  var duration = 10000
 
-  var iso = new Isomer(canvas)
-
-  var nRows = 20
   var nCols = 20
-  var nPlanes = 50
-  var nCells = nRows * nCols
-  var unit = 6/nRows
-
-  var oWidth = nCols/5
-  var oHeight = nRows/5
-  var oDist = oWidth/2
+  var nRows = 20
 
   function render (p) {
     ctx.clearRect(0, 0, width, height)
+    var mod = Math.sin(p * 2 * Math.PI) / 2 + 0.5
+    var tW = width/2
+    var tH = tW
+    var x0 = width/2 - tW/2
+    var y0 = height/2 - tH/2
 
-    if (p < 0.5) {
-      var p = ease.inOutSine(p*2)
-    } else {
-      var p = ease.inOutSine((1-p)*2)
-    }
+    var w = tW/nCols
+    var h = tH/nRows
 
-    var plane = Math.ceil(p * nPlanes)
+    for (var col = 0; col < nCols; col++) {
+      for (var row = 0; row < nRows; row++) {
+        var x = x0 + col * w
+        var y = y0 + row * h
 
-    var xPos = 0
-    var yPos = 0
-    var zPos = plane/nPlanes * oDist
+        var alpha = simplex.noise3d(col/12, row/12, mod*2)/2 + 0.5
 
-    for (var row = nRows; row > 0; row--) {
-      for (var col = nCols; col > 0; col--) {
-        var oX = xPos - (col/nCols) * oWidth
-        var oY = yPos - (row/nRows) * oHeight
-        var oZ = zPos
-
-        var mod = simplex.noise3d(oX, oY, oZ)/2 + 0.5
-
-        var w = unit
-        var d = unit
-        var h = unit/10
-
-        var x = row * w
-        var y = col * w
-        var z = plane * h
-
-        var pos = Point(x, y, z)
-        var c = cOrange.clone().lighten(1.2 * mod)
-        var ic = new Isomer.Color(c.red(), c.green(), c.blue())
-        var shape = Shape.Prism(pos, w, d, h)
-
-        iso.add(shape, ic)
+        ctx.fillStyle = 'rgba(255,255,255,'+alpha*alpha+')'
+        ctx.fillRect(x, y, 0.75 * w, 0.75 * h)
       }
     }
   }
@@ -82,10 +51,13 @@ module.exports = function(el) {
   function tick () {
     if (!canvas.parentNode) return
     var elapsed = Date.now() - timeStart
-    var p = (elapsed % duration) / duration
+    var p = (Date.now() % duration) / duration
     render(p)
     raf(tick)
   }
 
   tick()
 }
+
+
+
