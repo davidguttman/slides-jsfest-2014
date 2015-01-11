d104d84e9de4e0edc03eefc0fa7f14ff1d4e9934
David Guttman <david@davidguttman.com>
2015-01-11 09:23:51 -0800

    swapped wave pendulum movie out for youtube

--- slides/block-twist/actual.js
+++ slides/block-twist/actual.js
@@ -2,23 +2,9 @@ var h = require('hyperscript')
 
 module.exports = function(el) {
   el.innerHTML = ''
-  var video = render()
+  var video = document.createElement('div')
+  video.innerHTML = '<iframe width="640" height="480" src="//www.youtube.com/embed/yVkdfJ9PkRQ?rel=0" frameborder="0" allowfullscreen></iframe>'
   el.appendChild(video)
   var rect = video.getBoundingClientRect()
   video.style.marginTop = window.innerHeight/4 + 'px'
-
-  video.addEventListener('playing', function(evt) {
-    // video.currentTime = 50
-  })
-}
-
-function render () {
-  var html = h('video', {
-      src: '/images/wave-pendulum.mp4',
-      height: window.innerHeight/2,
-      audio: false,
-      autoplay: true
-    })
-
-  return html
 }
